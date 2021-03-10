fa273901af97ebfa00e5e7c1a1d5a9dc0d0455b2
DK36369 <71749676+DK36369@users.noreply.github.com>
2021-03-10 14:33:39 +0800

    Push the testcodeapp

--- CZ3003/Assets/Database/TestingCode_Create ConsoleApp.cs
+++ CZ3003/Assets/Database/TestingCode_Create ConsoleApp.cs
@@ -9,8 +9,25 @@ namespace Assets.Database
 {
     class TestingCode_Create_ConsoleApp
     {
-        DatabaseInit dbs = DatabaseInit.getInstance();
-        User tmp = new User("abc", "123", 11, 0, "Tom Dick Harry", "Adock@gmail.com");
-        dbs.createUser(tmp);
+        DatabaseInit db = DatabaseInit.getInstance();
+        User usr = null;
+        StageProgress a = new StageProgress(1, 1);
+        List<StageProgress> spl = new List<StageProgress>();
+        spl.Add(a);
+            SinglePlayer tmp = new SinglePlayer(spl);
+
+        GameHistory gh = new GameHistory(1, "abc", 1, DateTime.Now);
+        List<GameHistory> al = new List<GameHistory>();
+        al.Add(gh);
+
+            PVP t = new PVP(1, al);
+
+        User usrt = new User("tanasf", "abc,", tmp, 1, "tanahcaow", "a@gmail.com", t);
+        db.createUser(usrt);
+            Console.WriteLine("testing user");
+            Console.Write("Enter your name: ");
+
+            string name = Console.ReadLine();
+        Console.WriteLine($"Hello {name}");
     }
 }
