b85a8eb01617a7a2afb90f38314e3f2bd3437540
trakugane <56292786+trakugane@users.noreply.github.com>
2021-03-10 12:20:44 +0800

    Added Register
    Added script to RegisterUI

--- CZ3003/Assets/Scripts/AccountManager.cs
+++ CZ3003/Assets/Scripts/AccountManager.cs
@@ -14,7 +14,7 @@ public class AccountManager : MonoBehaviour
         
     }
 
-    public bool validateUser(InputField user, InputField pwd)
+    public bool validateUser(InputField email, InputField password)
     {
         // If username in database and password is correct, return true
         // Else, Return false
@@ -38,5 +38,25 @@ public class AccountManager : MonoBehaviour
         PlayerPrefs.SetString("accountType", accountType);
 
         return true;
+    }
+
+    public int validateAccountCreation(InputField email, InputField password, InputField confirmPassword, InputField personalName)
+    {
+        // Check Email with database
+        // Check Password & Confirm Password is the same
+        if (password.text.ToString() != confirmPassword.text.ToString())
+            return 2;
+
+        // Store all details into database
+        int currProg = 11;
+        // Check if email is student or teacher email
+        string accountType = "S";
+        // int tutorial = 0; check if havent done tutorial?
+
+        // If store unsuccessful, return 3
+
+
+        /*Debug.Log("Error: Account Creation");*/
+        return 0;
     }
 }
---
+++ CZ3003/Assets/Scripts/RegisterUI.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.Events;
+
+public class RegisterUI : MonoBehaviour
+{
+    public InputField email;
+    public InputField password;
+    public InputField confirmPassword;
+    public InputField personalName;
+
+    public Button btnRegister;
+
+    AccountManager am;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        am = gameObject.AddComponent<AccountManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void checkRegister(string sceneName)
+    {
+        Debug.Log("Register Button Pressed!");
+        int codeMsg = am.validateAccountCreation(email, password, confirmPassword, personalName);
+        if (codeMsg == 0)
+            SceneManager.LoadScene(sceneName);
+        else
+        {
+            // Need display error message to UI (10/3/21)
+            Debug.Log("Error: Account Creation");
+            if (codeMsg == 1)
+            {
+                Debug.Log("Error: Email already exists");
+            }
+            else if (codeMsg == 2)
+            {
+                Debug.Log("Error: Passwords don't match");
+            }
+            else if (codeMsg == 3)
+            {
+                Debug.Log("Error: Storing in database unsuccessful");
+            }
+            else
+            {
+                Debug.Log("Error: Unknown Error");
+            }
+        }
+            
+    }
+
+    
+}
