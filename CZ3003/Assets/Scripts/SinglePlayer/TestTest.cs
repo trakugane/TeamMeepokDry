1e9b87b631e95f4bd50bfb045293fe9613019999
shxnying <62127289+shxnying@users.noreply.github.com>
2021-03-15 12:39:37 +0800

    Set of Questions for single player world

--- CZ3003/Assets/Scripts/SinglePlayer/TestTest.cs
+++ CZ3003/Assets/Scripts/SinglePlayer/TestTest.cs
@@ -18,8 +18,31 @@ public class TestTest : MonoBehaviour
     public GameObject ScoreText;
     int playerscore = 0;
     
-    public string[] QnA = new string[] { "6+1=", "4+5=", "3+5=" , "0+0=" , "2+3="};
+    //addition
+    public string[] QnA11 = new string[] { "6+1=", "4+5=", "3+5=" , "0+0=" , "2+3="};
+    public string[] QnA12 = new string[] { "6+7=", "4+9=", "5+8=", "9+9=", "7+9=" };
+    public string[] QnA13 = new string[] { "13+1=", "5+16=", "23+54=", "70+1=", "7+17=" };
+    public string[] QnA14 = new string[] { "8+12=", "55+33=", "77+4=", "29+14=", "0+32=" };
 
+    //subtraction
+    public string[] QnA21 = new string[] { "4-3=", "8-5=", "10-2=", "1-0=", "9-4=" };
+    public string[] QnA22 = new string[] { "18-8=", "14-11=", "25-8=", "17-8=", "24-7=" };
+    public string[] QnA23 = new string[] { "35-16=", "45-29=", "16-4=", "88-32=", "60-32=" };
+    public string[] QnA24 = new string[] { "18-12=", "55-43=", "37-23=", "85-39=", "92-42=" };
+
+    //multiplication
+    public string[] QnA31 = new string[] { "4x7=", "8x6=", "10x2=", "5x0=", "9x8=" };
+    public string[] QnA32 = new string[] { "18x2=", "11x4=", "2x14=", "15x5=", "7x9=" };
+    public string[] QnA33 = new string[] { "3x8=", "4x14=", "17x3=", "10x12=", "16x0=" };
+    public string[] QnA34 = new string[] { "1x80=", "5x7=", "3x12=", "12x12=", "8x9=" };
+
+    //division
+    public string[] QnA41 = new string[] { "5/1=", "6/3=", "10/2=", "14/7=", "6/2=" };
+    public string[] QnA42 = new string[] { "18/2=", "16/2=", "16/4=", "15/5=", "90/10=" };
+    public string[] QnA43 = new string[] { "8/8=", "32/8=", "72/3=", "66/11=", "40/5=" };
+    public string[] QnA44 = new string[] { "55/5=", "36/6=", "32/4=", "96/12=", "88/8=" };
+
+    public string[] tempQnA;
 
     public int index;
     public string currentQuestion; // + selectedstagevalue;
@@ -34,8 +57,78 @@ public class TestTest : MonoBehaviour
     public int gamewin = 3; //default, havent play before
     public static int size = 5;
 
+    //TODO Fetch world
+    public int world = 34;
+
     void Start()
     {
+        checkWorld();
+    }
+
+    public void checkWorld()
+    {
+        if (world < 20)
+        {
+            if (world == 11)
+                tempQnA = QnA11;
+            else if (world == 12)
+                tempQnA = QnA12;
+            else if (world == 13)
+                tempQnA = QnA13;
+            else if (world == 14)
+                tempQnA = QnA14;
+            else
+                //boss level
+                //just use any world level first
+                tempQnA = QnA14;
+
+        }
+        else if (world<30)
+        {
+            if (world == 21)
+                tempQnA = QnA21;
+            else if (world == 22)
+                tempQnA = QnA22;
+            else if (world == 23)
+                tempQnA = QnA23;
+            else if (world == 24)
+                tempQnA = QnA24;
+            else
+                //boss level
+                //just use any world level first
+                tempQnA = QnA24;
+        }
+        else if (world < 40)
+        {
+            if (world == 31)
+                tempQnA = QnA31;
+            else if (world == 32)
+                tempQnA = QnA32;
+            else if (world == 33)
+                tempQnA = QnA33;
+            else if (world == 34)
+                tempQnA = QnA34;
+            else
+                //boss level
+                //just use any world level first
+                tempQnA = QnA34;
+        }
+        else
+        {
+            if (world == 41)
+                tempQnA = QnA41;
+            else if (world == 42)
+                tempQnA = QnA42;
+            else if (world == 43)
+                tempQnA = QnA43;
+            else if (world == 44)
+                tempQnA = QnA44;
+            else
+                //boss level
+                //just use any world level first
+                tempQnA = QnA44;
+        }
+
         generateQuestion();
     }
 
@@ -48,17 +141,17 @@ public class TestTest : MonoBehaviour
         else if (gamewin == 2)
             size--;
 
-        Debug.Log("gamewin generateQn= " + gamewin);
-        for ( index = 0; index < size; index++)
+
+        for (index = 0; index < size; index++)
         {
-            
+
             Debug.Log("current index " + index);
-            currentQuestion = QnA[index];
+            currentQuestion = tempQnA[index];
             fetchQn();
         }
 
-        
     }
+
     public void fetchQn()
     {
         for (int i = 0; i < currentQuestion.Length; i++)
@@ -178,6 +271,7 @@ public class TestTest : MonoBehaviour
         Text btnText = btn.GetComponentInChildren<Text>();
         int noFromButton = int.Parse(btnText.text);
         Debug.Log("number from button is " + noFromButton);
+        int count = 5;
 
         if (noFromButton == ans)
         {
@@ -190,7 +284,11 @@ public class TestTest : MonoBehaviour
             Debug.Log("Player score: " +playerscore);
             //Debug.Log(GameController.gameController.points4game);
             //Destroy(GameController.gameController.questionPanel);
-            generateQuestion();
+            count--;
+            if (count >0)
+                generateQuestion();
+            //else
+                //Go to score page
         }
         else
         {
@@ -200,7 +298,11 @@ public class TestTest : MonoBehaviour
             Debug.Log("u clicked on the wrong answer : " + noFromButton);
 
             Debug.Log("Player score: " + playerscore);
-            generateQuestion();
+            count--;
+            if (count > 0)
+                generateQuestion();
+            //else
+                //Go to score page
         }
     }
 
