b9c749e6fb71eb640555aa0f85cbbe9bacbebc8e
shxnying <62127289+shxnying@users.noreply.github.com>
2021-03-31 16:14:20 +0800

    Social Media Sharing : FB & TW can share

---
+++ CZ3003/Assets/Scripts/SocialMediaSharing/SocialSharing.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SocialSharing : MonoBehaviour
+{
+
+	//Twitter variables
+	string TWITTER_ADDRESS = "http://twitter.com/intent/tweet";
+	string TWEET_LANGUAGE = "en";
+    string textToDisplay = "Hey Guys! Check out my score: ";
+
+
+
+	//App ID
+	string AppID = "805120583426252";
+
+	//This link is attached to this post
+	string Link = "https://www.facebook.com/TeamMeePokDry/about/?ref=page_internal";
+
+	//The URL of a picture attached to this post. 
+	//The Size must be atleat 200px by 200px.
+	string Picture = "http://i-cdn.phonearena.com/images/article/85835-thumb/Google-Pixel-3-codenamed-Bison-to-be-powered-by-Andromeda-OS.jpg";
+
+	string Caption = "Check out My New Score: ";
+	string Description = "Enjoy Fun, free games! Challenge yourself or share with friends. Fun and easy to use games.";
+
+
+	// Twitter Share Button	
+	public void shareScoreOnTwitter()
+	{
+
+		//Can modify by level or assignment
+		string nameParameter = "ASSIGNMENT COMPLETED!";//this is limited in text length 
+		Application.OpenURL(TWITTER_ADDRESS +
+		   "?text=" + WWW.EscapeURL(textToDisplay + "\n\nGet the Game:\n" + Link + "\n\n#TeamMeePokDryGame #CZ3003"));
+
+	}
+
+	// Facebook Share Button
+	public void shareScoreOnFacebook()
+	{
+		Application.OpenURL("https://www.facebook.com/dialog/feed?" + "app_id=" + AppID + "&link=" + Link + "&picture=" + Picture
+							 + "&caption=" + Caption /*+ score.points*/ + "&description=" + Description);
+	}
+}
---
+++ CZ3003/Assets/Scripts/SocialMediaSharing/TwitterScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TwitterScript : MonoBehaviour
+{
+    private const string TWITTER_ADDRESS = "http://twitter.com/intent/tweet";
+    private const string TWEET_LANGUAGE = "en";
+    public static string descriptionParam;
+    private string appStoreLink = "https://www.facebook.com/TeamMeePokDry/about/?ref=page_internal";
+
+    public void ShareToTW(string linkParameter)
+    {
+        //Can modify by level or assignment
+        string nameParameter = "ASSIGNMENT COMPLETED!";//this is limited in text length 
+        Application.OpenURL(TWITTER_ADDRESS +
+           "?text=" + WWW.EscapeURL(nameParameter + "\n" + descriptionParam + "\n" + "Get the Game:\n" + appStoreLink +"\n#TeamMeePokDryGame #CZ3003"));
+    }
+}
